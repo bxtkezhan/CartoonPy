7df3610b737e600027f0349de58ef083cea062f4
bxtkezhan <bxtkezhan@qq.com>
2016-11-18 17:04:15 +0800

    set yapf disable

--- cartoonpy/video/compositing/concatenate.py
+++ cartoonpy/video/compositing/concatenate.py
@@ -84,12 +84,11 @@ def concatenate_videoclips(clips, method="chain", transition=None,
 
         result = VideoClip(ismask=ismask, make_frame=make_frame)
         if any([c.mask is not None for c in clips]):
-            masks = [
-                c.mask if (c.mask is not None) else ColorClip(
-                    [1, 1], col=1, ismask=True, duration=c.duration)
-                #ColorClip(c.size, col=1, ismask=True).set_duration(c.duration)
-                for c in clips
-            ]
+            #ColorClip(c.size, col=1, ismask=True).set_duration(c.duration)
+            masks = [c.mask
+                     if (c.mask is not None)
+                     else ColorClip( [1, 1], col=1, ismask=True, duration=c.duration)
+                     for c in clips] # yapf: disable
             result.mask = concatenate_videoclips(
                 masks, method="chain", ismask=True)
             result.clips = clips
